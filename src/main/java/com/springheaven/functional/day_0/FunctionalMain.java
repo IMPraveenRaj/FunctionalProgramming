5bcabda4e02966e9d6e9ff7427b21047f447b5bc
IMPraveenRaj <impraveen13@gmail.com>
2025-02-08 20:02:59 +0100

    initial changes

--- src/main/java/com/springheaven/functional/day_0/FunctionalMain.java
+++ src/main/java/com/springheaven/functional/day_0/FunctionalMain.java
@@ -1,9 +1,29 @@
 package com.springheaven.functional.day_0;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class FunctionalMain {
 
 
+    private String name;
+
+
+
+    public List<String> getMeListOfNames(){
+        return   List.of("PraveenRaj","MohanRaj","VasanthiraDevi");
+    }
+
     public static void main(String[] args) {
+        FunctionalMain fm = new FunctionalMain();
         System.out.println("Hello World");
+        List<String> meListOfNames = new FunctionalMain().getMeListOfNames();
+        Consumer<String> stringConsumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("i am the implementation of the consumer interface"+s);
+            }
+        };
+        meListOfNames.forEach(stringConsumer);
     }
 }
