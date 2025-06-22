a6efc29217f985eb4ff294a7ede462083abbebc2
IMPraveenRaj <impraveen13@gmail.com>
2025-06-22 18:44:14 +0200

    map and flat map changes

--- src/main/java/com/springheaven/functional/day_01/MapVsFlatMap.java
+++ src/main/java/com/springheaven/functional/day_01/MapVsFlatMap.java
@@ -1,2 +1,30 @@
-package com.springheaven.functional.day_01;public class MapVsFlatMap {
+package com.springheaven.functional.day_01;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapVsFlatMap {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("praveen", List.of("1234566789", "098756634"));
+        Student student2 = new Student("Dharshini", List.of("1234567890", "098878333"));
+
+        List<List<String>> praveenNo = List.of(student,student2)
+                .stream()
+                .filter(student1 -> student1.getName().contains("praveen"))
+                .map(student1 -> student1.getPhoneNo())
+                .collect(Collectors.toList());
+
+        System.out.println(praveenNo);
+
+        List<String> allPhoneNo = List.of(student,student2)
+                .stream()
+                .filter(student1 -> student1.getName().contains("Dharshini"))
+                .flatMap(student1 -> student1.getPhoneNo().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("Everyone phone no "+allPhoneNo);
+
+    }
 }
--- src/main/java/com/springheaven/functional/day_01/Student.java
+++ src/main/java/com/springheaven/functional/day_01/Student.java
@@ -1,2 +1,22 @@
-package com.springheaven.functional.day_01;public class Student {
+package com.springheaven.functional.day_01;
+
+import java.util.List;
+
+public class Student {
+
+    private String name;
+    private List<String> phoneNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getPhoneNo() {
+        return phoneNo;
+    }
+
+    public Student(String name, List<String> phoneNo) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+    }
 }
