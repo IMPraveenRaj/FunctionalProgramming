0949b0790ffbc39f65be43a5ccc81df7afa4cd05
IMPraveenRaj <impraveen13@gmail.com>
2025-07-27 11:23:40 +0200

    refactoring changes

--- src/main/java/com/springheaven/functional/MainLabs.java
+++ src/main/java/com/springheaven/functional/MainLabs.java
@@ -9,8 +9,6 @@ import java.util.List;
 public class MainLabs {
 
     //This class built fo the tutorial purpose of my student
-
-
     public static void main(String[] args) {
         String family[][] = {
                 {"1", "praveen", "Billionaire"},
@@ -36,6 +34,4 @@ public class MainLabs {
                 .flatMap(newfam-> Arrays.stream(newfam))
                 .forEach(fami -> System.out.println(fami));
     }
-
-
 }
